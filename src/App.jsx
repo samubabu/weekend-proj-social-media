3adeb550de03f4679d9ae133b6357f7dc519e35e
Sam B <samb@Sams-MacBook-Pro.local>
2022-11-16 11:36:59 -0600

    commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from "react-flask-app/src/components/Navbar.jsx"
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Navbar(props) {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Welcome</a>
+            </div>
+
+        </nav>
+    )
+}
