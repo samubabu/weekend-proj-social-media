5adbe7e84695b6a67927b2c99fc4491a56ed9ff0
Sam B <samb@Sams-MacBook-Pro.local>
2022-11-19 10:28:36 -0600

    some debugging while doing final project

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import {useNavigate}  from 'react-router-dom';
 
 export default function Login(props) {
+
+    const navigate=useNavigate();
+
     const handleSubmit =event=>{
         event.preventDefault();
         console.log(event);
+
+//get data from form
+let username=event.target.username.value;
+let password=event.target.password.value;
+let stringToEncode=`${username}:${password}`
+//console.log(username,password)
+//to encode this username and password we use btoa
+//console.log(btoa(`${username}:${password}`))
+
+let myHeaders =new Headers();
+myHeaders.append('Authorization',`Basic ${btoa(stringToEncode)}`)
+
+fetch('https://kekambas-blog.herokuapp.com/auth/token', {
+    method: 'POST',
+    headers: myHeaders
+})
+        .then(res=>res.json())
+        //instead of logging the data, a local storage built  can be used
+        //.then (data=>console.log(data))
+        .then(data=>{
+            //get token from the response
+            let token =data.token;
+            //store values in local stoarge on the browser
+            localStorage.setItem('token',token);
+            //redirect back to home back
+            navigate()
+        })
+
     }
   return (
     <>
